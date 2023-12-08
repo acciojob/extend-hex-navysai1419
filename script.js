91befa87051528d668a25cdf5942bba84baa170f
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 13:12:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const extendHex = (shortHex) => {
-  // write your code here
-    const fullHex = `${shortHex[0]}${shortHex[0]}${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}`;
-	return `#${fullHex}`;
+  // Remove '#' symbol if present
+  shortHex = shortHex.replace('#', '');
+
+  // Extend each color component to get the full hex code
+  const fullHex = `${shortHex[0]}${shortHex[0]}${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}`;
+
+  // Add the '#' symbol back and return the full hex code
+  return `#${fullHex}`;
 };
 
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));  
+alert(extendHex(shortHex));
