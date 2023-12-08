895bd591d1c1b688aac6982b57c49a1ed746a4f1
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 12:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const extendHex = (shortHex) => {
   // write your code here
+    const fullHex = `${shortHex[0]}${shortHex[0]}${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}`;
+	return `#${fullHex}`;
 };
 
-// Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendHex(shortHex));  
